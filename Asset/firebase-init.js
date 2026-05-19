2a44f038f7e00a0b89315131871acd323c144b63
mkhoirulzed-svg <mkhoirulzed@gmail.com>
2026-05-19 09:06:07 +0700

    Update firebase-init.js

--- Asset/firebase-init.js
+++ Asset/firebase-init.js
@@ -6,57 +6,14 @@ import {
   onMessage
 } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-messaging.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCj_KSdwjSZy3v2D1Wry1VDxBlFTS4d518",
-  authDomain: "zedkalkulator.firebaseapp.com",
-  projectId: "zedkalkulator",
-  storageBucket: "zedkalkulator.firebasestorage.app",
-  messagingSenderId: "224764172",
-  appId: "1:224764172:web:559f67b313bd782dba2929",
-  measurementId: "G-WEY9ZV12D1"
-};
-
-const app = initializeApp(firebaseConfig);
-const messaging = getMessaging(app);
-
-// Minta izin notifikasi
-Notification.requestPermission().then(async (permission) => {
-
-  if (permission === "granted") {
-
-    const token = await getToken(messaging, {
-      vapidKey: "BEJ4JTcnkkQDPZRSaoOWmde04kLDHRv7XfQXyr0LZU3VPFAyWMX_-nT6dlp17HlT_CFycmGvZBYJJbYl37_ju0A"
-    });
-
-    console.log("TOKEN:", token);
-
-    alert("Notifikasi berhasil diaktifkan!");
-
-  } else {
-    alert("Izin notifikasi ditolak");
-  }
-
-});
-
-// Saat notif diterima
-onMessage(messaging, (payload) => {
-  console.log("Pesan diterima:", payload);
-});
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
-
-import {
-  getMessaging,
-  getToken,
-  onMessage
-} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-messaging.js";
-
 import {
   getFirestore,
   doc,
   setDoc,
   serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
+
+alert("Firebase init jalan");
 
 const firebaseConfig = {
   apiKey: "AIzaSyCj_KSdwjSZy3v2D1Wry1VDxBlFTS4d518",
@@ -77,7 +34,7 @@ async function aktifkanNotifikasi() {
     const permission = await Notification.requestPermission();
 
     if (permission !== "granted") {
-      console.log("Izin notifikasi ditolak");
+      alert("Izin notifikasi ditolak");
       return;
     }
 
@@ -86,7 +43,7 @@ async function aktifkanNotifikasi() {
     });
 
     if (!token) {
-      console.log("Token tidak ditemukan");
+      alert("Token tidak ditemukan");
       return;
     }
 
@@ -97,10 +54,10 @@ async function aktifkanNotifikasi() {
       updatedAt: serverTimestamp()
     });
 
-    console.log("Token berhasil disimpan:", token);
+    alert("Token berhasil disimpan ke Firestore");
 
   } catch (error) {
-    console.error("Gagal mengaktifkan notifikasi:", error);
+    alert("Gagal: " + error.message);
   }
 }
 
