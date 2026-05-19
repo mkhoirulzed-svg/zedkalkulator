edfc3db6686994c8c3f59c9eec5a7c9d1763290a
mkhoirulzed-svg <mkhoirulzed@gmail.com>
2026-05-19 08:35:01 +0700

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+importScripts('https://www.gstatic.com/firebasejs/12.13.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/12.13.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCj_KSdwjSZy3v2D1Wry1VDxBlFTS4d518",
+  authDomain: "zedkalkulator.firebaseapp.com",
+  projectId: "zedkalkulator",
+  storageBucket: "zedkalkulator.firebasestorage.app",
+  messagingSenderId: "224764172",
+  appId: "1:224764172:web:559f67b313bd782dba2929",
+  measurementId: "G-WEY9ZV12D1"
+});
+
+const messaging = firebase.messaging();
