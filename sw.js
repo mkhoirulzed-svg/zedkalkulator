8693dbcb8ca6c03553fa9f38dbfea20f185d21d7
mkhoirulzed-svg <mkhoirulzed@gmail.com>
2026-05-19 21:43:00 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,39 @@
-self.addEventListener("install", () => {
+const CACHE_NAME = "zedkalkulator-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/192x192.png",
+  "/512x512.png"
+];
+
+self.addEventListener("install", (event) => {
   self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 });
 
-self.addEventListener("activate", () => {
-  clients.claim();
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
