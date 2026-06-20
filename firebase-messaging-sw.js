98ad38542fdc4133ad141951daa192d616a85848
mkhoirulzed-svg <mkhoirulzed@gmail.com>
2026-06-20 19:34:56 +0700

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,3 +1,17 @@
+importScripts('https://www.gstatic.com/firebasejs/12.13.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/12.13.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCj_KSdwjSZy3v2D1Wry1VDxBlFTS4d518",
+  authDomain: "zedkalkulator.firebaseapp.com",
+  projectId: "zedkalkulator",
+  storageBucket: "zedkalkulator.firebasestorage.app",
+  messagingSenderId: "224764172",
+  appId: "1:224764172:web:559f67b313bd782dba2929"
+});
+
+const messaging = firebase.messaging();
+
 messaging.onBackgroundMessage((payload) => {
   console.log("FCM background:", payload);
 
