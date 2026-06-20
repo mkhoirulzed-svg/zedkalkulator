247851f8c6eaf399b5523e91efa6364fc6ff7024
mkhoirulzed-svg <mkhoirulzed@gmail.com>
2026-06-20 19:33:02 +0700

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,22 +1,22 @@
-importScripts('https://www.gstatic.com/firebasejs/12.13.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/12.13.0/firebase-messaging-compat.js');
+messaging.onBackgroundMessage((payload) => {
+  console.log("FCM background:", payload);
 
-firebase.initializeApp({
-  apiKey: "AIzaSyCj_KSdwjSZy3v2D1Wry1VDxBlFTS4d518",
-  authDomain: "zedkalkulator.firebaseapp.com",
-  projectId: "zedkalkulator",
-  storageBucket: "zedkalkulator.firebasestorage.app",
-  messagingSenderId: "224764172",
-  appId: "1:224764172:web:559f67b313bd782dba2929",
-  measurementId: "G-WEY9ZV12D1"
-});
+  // Kalau payload dari Firebase Console sudah punya notification,
+  // jangan tampilkan ulang agar tidak dobel
+  if (payload.notification) {
+    return;
+  }
 
-const messaging = firebase.messaging();
+  const title = payload.data?.title || "ZED Kalkulator";
 
-self.addEventListener("notificationclick", (event) => {
-  event.notification.close();
+  const options = {
+    body: payload.data?.body || "Ada update terbaru.",
+    icon: "https://zedkalkulator.site/192x192.png",
+    badge: "https://zedkalkulator.site/192x192.png",
+    data: {
+      url: payload.data?.url || "https://zedkalkulator.site"
+    }
+  };
 
-  event.waitUntil(
-    clients.openWindow("https://zedkalkulator.site")
-  );
+  return self.registration.showNotification(title, options);
 });
